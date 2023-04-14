8cc3bb5bd5369062f2ee36399d205e88e4f54a93
GPIOX <1193537533@qq.com>
2023-04-14 13:21:38 +0800

    更新配置文件

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# test
+# 基于PyTorch Lightning上设计的模板代码
+⭐ 初衷是为了做实验时尽量的减少些代码量
+
+## 1. 文件构成and说明
+### 1.1 文件构成
+```bash
+├── config
+│   ├── base_config.py
+│   ├── cifar10_resnet.py
+│   └── config_parsing.py
+├── dataset
+│   ├── cifar_dataset.py
+│   ├── dataloader.py
+│   ├── __init__.py
+│   └── utils.py
+├── main.py
+├── model
+│   ├── base_loss.py
+│   ├── base_trainer.py
+│   ├── __init__.py
+│   ├── network.py
+│   ├── pretrained
+│   └── resnet
+│       ├── resnet.py
+│       └── trainer.py
+├── README.md
+└── tools
+    └── utils.py
+```
+### 1.2 说明
+⚙️ 配置文件 
++ ├── config 
+  +  ├── base_config.py 
+  +  ├── cifar10_resnet.py
+  +  └── config_parsing.py
--- config/cifar10_resnet.py
+++ config/cifar10_resnet.py
@@ -1,135 +1,137 @@
-pl_trainer_func = dict(
-    max_epochs=250,
-    accelerator='gpu',
-    devices=1,
-    precision=32,  # 精度
-    # logger = logger # 日志管理器
-    val_check_interval=1.0, # 设置训练完一个epoch后才开始验证
-)
+_base_ = ['./base_config.py']
 
-model = dict(
-    name='ResNet18',
-    model_arg=dict(
-        input_channel=3,
-        n_outputs=10,
-    ),
+# pl_trainer_func = dict(
+#     max_epochs=250,
+#     accelerator='gpu',
+#     devices=1,
+#     precision=32,  # 精度
+#     # logger = logger # 日志管理器
+#     val_check_interval=1.0, # 设置训练完一个epoch后才开始验证
+# )
 
-    # 设置训练模型相关的参数，如优化器、学习率等
-    trainer=dict(
-        epoch=pl_trainer_func['max_epochs'],
-        device=pl_trainer_func[
-            'devices'],  # devices > 1时自定义的trainer中用来对logger操作用
-        name='ResNetTrainer',
-        trainer_arg=dict(
-            # 定义损失函数
-            loss=dict(
-                name='CustomLoss',
-                weight=None,
-                loss_1=dict(
-                    name='CrossEntropyLoss',
-                    loss_arg = dict(reduce=False, ignore_index=-1),
-                ),
+# model = dict(
+#     name='ResNet18',
+#     model_arg=dict(
+#         input_channel=3,
+#         n_outputs=10,
+#     ),
 
-            ),
-            # 定义优化器
-            optimizer=dict(name='SGD',
-                           optimizer_arg=dict(
-                               lr=0.01,
-                               momentum=0.9,
-                               weight_decay=5E-4,
-                           )),
+#     # 设置训练模型相关的参数，如优化器、学习率等
+#     trainer=dict(
+#         epoch=pl_trainer_func['max_epochs'],
+#         device=pl_trainer_func[
+#             'devices'],  # devices > 1时自定义的trainer中用来对logger操作用
+#         name='ResNetTrainer',
+#         trainer_arg=dict(
+#             # 定义损失函数
+#             loss=dict(
+#                 name='CustomLoss',
+#                 weight=None,
+#                 loss_1=dict(
+#                     name='CrossEntropyLoss',
+#                     loss_arg = dict(reduce=False, ignore_index=-1),
+#                 ),
 
-            # 定义学习率调整
-            lr_scheduler=dict(
-                name='MultiStepLR',
-                scheduler_arg=dict(
-                    milestones=[
-                        int(pl_trainer_func['max_epochs'] * 0.25),
-                        int(pl_trainer_func['max_epochs'] * 0.5),
-                        int(pl_trainer_func['max_epochs'] * 0.75),
-                    ],
-                    gamma=0.5,
-                ),
-            ),
-        ),
-    ),
-)
+#             ),
+#             # 定义优化器
+#             optimizer=dict(name='SGD',
+#                            optimizer_arg=dict(
+#                                lr=0.01,
+#                                momentum=0.9,
+#                                weight_decay=5E-4,
+#                            )),
 
-# 定义训练集相关参数
-train_dataset = dict(
-    name='CIFAR10',
-    dataset_arg=dict(
-        root='./data',
-        download=True,
-        train=True,
-        noise_type='pairflip',
-        noise_rate=0.6,
-    ),
-    dataloader_arg=dict(
-        batch_size=128,
-        num_workers=4,
-        shuffle=True,
-        # pin_memory = true
-    ),
-    transform=dict(
-        RandomCrop=dict(
-            size=32,
-            padding=4,
-        ),
-        RandomHorizontalFlip=None,
-        ToTensor=None,
-    ),
-)
+#             # 定义学习率调整
+#             lr_scheduler=dict(
+#                 name='MultiStepLR',
+#                 scheduler_arg=dict(
+#                     milestones=[
+#                         int(pl_trainer_func['max_epochs'] * 0.25),
+#                         int(pl_trainer_func['max_epochs'] * 0.5),
+#                         int(pl_trainer_func['max_epochs'] * 0.75),
+#                     ],
+#                     gamma=0.5,
+#                 ),
+#             ),
+#         ),
+#     ),
+# )
 
-# 定义测试集相关参数
-test_dataset = dict(
-    name='CIFAR10',
-    dataset_arg=dict(
-        root='./data',
-        download=True,
-        train=False,
-        noise_type='pairflip',
-        noise_rate=0.2,
-    ),
-    dataloader_arg=dict(
-        batch_size=128,
-        num_workers=4,
-        shuffle=False,
-        # pin_memory = true
-    ),
-    transform=dict(ToTensor=None, ),
-)
+# # 定义训练集相关参数
+# train_dataset = dict(
+#     name='CIFAR10',
+#     dataset_arg=dict(
+#         root='./data',
+#         download=True,
+#         train=True,
+#         noise_type='pairflip',
+#         noise_rate=0.6,
+#     ),
+#     dataloader_arg=dict(
+#         batch_size=128,
+#         num_workers=4,
+#         shuffle=True,
+#         # pin_memory = true
+#     ),
+#     transform=dict(
+#         RandomCrop=dict(
+#             size=32,
+#             padding=4,
+#         ),
+#         RandomHorizontalFlip=None,
+#         ToTensor=None,
+#     ),
+# )
 
-# 定义断点保存
-callbacks = dict(
-    ModelCheckpoint1_arg=dict(
-        save_top_k=1,
-        monitor="valid_loss",
-        filename='{epoch}-{valid_loss:.3f}',
-    ),
-    ModelCheckpoint2_arg=dict(
-        save_top_k=1,
-        monitor="valid_acc",
-        filename='{epoch}-{valid_acc:.3f}',
-        mode="max",
-    ),
-)
+# # 定义测试集相关参数
+# test_dataset = dict(
+#     name='CIFAR10',
+#     dataset_arg=dict(
+#         root='./data',
+#         download=True,
+#         train=False,
+#         noise_type='pairflip',
+#         noise_rate=0.2,
+#     ),
+#     dataloader_arg=dict(
+#         batch_size=128,
+#         num_workers=4,
+#         shuffle=False,
+#         # pin_memory = true
+#     ),
+#     transform=dict(ToTensor=None, ),
+# )
 
-# 定义logger
-logger = dict(
-    logger1=dict(
-        name='CSVLogger',
-        logger_arg=dict(
-            save_dir="experment_logs",
-            name=f"CSVLog",
-            flush_logs_every_n_steps=1
-        ),
-    ),
-    logger2=dict(
-        name='TensorBoardLogger',
-        logger_arg=dict(
-            save_dir="experment_logs",
-            name=f"TensorBoardLog",
-        ),
-    ),
-)
+# # 定义断点保存
+# callbacks = dict(
+#     ModelCheckpoint1_arg=dict(
+#         save_top_k=1,
+#         monitor="valid_loss",
+#         filename='{epoch}-{valid_loss:.3f}',
+#     ),
+#     ModelCheckpoint2_arg=dict(
+#         save_top_k=1,
+#         monitor="valid_acc",
+#         filename='{epoch}-{valid_acc:.3f}',
+#         mode="max",
+#     ),
+# )
+
+# # 定义logger
+# logger = dict(
+#     logger1=dict(
+#         name='CSVLogger',
+#         logger_arg=dict(
+#             save_dir="experment_logs",
+#             name=f"CSVLog",
+#             flush_logs_every_n_steps=1
+#         ),
+#     ),
+#     logger2=dict(
+#         name='TensorBoardLogger',
+#         logger_arg=dict(
+#             save_dir="experment_logs",
+#             name=f"TensorBoardLog",
+#         ),
+#     ),
+# )
